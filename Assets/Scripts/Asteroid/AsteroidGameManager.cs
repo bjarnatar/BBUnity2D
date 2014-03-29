6b86104afd0887d7894a1add13e35b0a34172528
Bjarne Rene <bjarne@vostopia.com>
2014-03-29 16:01:44 +0100

    Asteroids spawning

--- Assets/Scripts/Asteroid/AsteroidGameManager.cs
+++ Assets/Scripts/Asteroid/AsteroidGameManager.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AsteroidGameManager : MonoBehaviour {
+public class AsteroidGameManager : MonoBehaviour
+{
+	public Vector2 stageArea = new Vector2(7.5f, 5.5f);
+
+	public int startingAsteroids = 5;
+	public float spawnFrequency = 5f;
+	public float spawnVelocity = 2f;
+
+	public Rigidbody2D asteroidPrefab;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		for (int i = 0 ; i < 5 ; ++i)
+		{
+			SpawnAsteroid();
+		}
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	// TODO Make the "spawn frame" narrower around the screen. Build in a little buffer around the play area
+	// TODO Make wrap around trigger after asteroids have entered the screen
+	void SpawnAsteroid()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		float distanceToPlayer = 0;
+		float spawnPosX = Random.Range(-stageArea.x, stageArea.x);
+		if (spawnPosX > 0)
+		{
+			spawnPosX += stageArea.x;
+		}
+		float spawnPosY = Random.Range(-stageArea.y, stageArea.y);
+		if (spawnPosY > 0)
+		{
+			spawnPosY += stageArea.y;
+		}
+		Vector2 spawnPos = new Vector2(spawnPosX, spawnPosY);
+		Vector2 targetPos = new Vector2(Random.Range (0, stageArea.x), Random.Range(0, stageArea.y));
+
+		Rigidbody2D asteroid = (Rigidbody2D) Instantiate(asteroidPrefab, spawnPos, transform.rotation);
+		asteroid.velocity = (targetPos - spawnPos).normalized * spawnVelocity;
 	}
 }
--- Assets/Scripts/Asteroid/AsteroidStone.cs
+++ Assets/Scripts/Asteroid/AsteroidStone.cs
@@ -3,16 +3,47 @@ using System.Collections;
 
 public class AsteroidStone : MonoBehaviour
 {
+	public float maxStartingVelocity;
+
+	public Rigidbody2D asteroidPrefabNextSizeDown;
+	public float newAsteroidVelocity = 0.5f;
+	public float asteroidExitAngle = 60.0f;
+	public float newAsteroidStartingDistance = 1f;
 
 	// Use this for initialization
 	void Start ()
 	{
 	
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+//	static Vector2 Make2D(Vector3 input)
+//	{
+//
+//	}
+
+	void OnCollisionEnter2D(Collision2D other)
 	{
+		if (other.gameObject.tag == "Bullet")
+		{
+			// Destroy this asteroid and the bullet
+			Object.Destroy(gameObject);
+			Object.Destroy(other.gameObject);
+
+			// If there is a prefab for the next size down, create two new asteroids (split in half)
+			if (asteroidPrefabNextSizeDown)
+			{
+				Vector3 instance1Direction = Quaternion.AngleAxis(asteroidExitAngle, Vector3.forward) * other.relativeVelocity.normalized;
+				Vector3 instance2Direction = Quaternion.AngleAxis(-asteroidExitAngle, Vector3.forward) * other.relativeVelocity.normalized;
+
+				Vector3 a1StartingPosition = transform.position + instance1Direction * newAsteroidStartingDistance;
+				Vector3 a2StartingPosition = transform.position + instance2Direction * newAsteroidStartingDistance;
+
+				Rigidbody2D asteroidInstance1 = (Rigidbody2D) Instantiate(asteroidPrefabNextSizeDown, a1StartingPosition, transform.rotation);
+				Rigidbody2D asteroidInstance2 = (Rigidbody2D) Instantiate(asteroidPrefabNextSizeDown, a2StartingPosition, transform.rotation);
 	
+				asteroidInstance1.velocity = instance1Direction * newAsteroidVelocity;
+				asteroidInstance2.velocity = instance2Direction * newAsteroidVelocity;
+			}
+		}
 	}
 }
